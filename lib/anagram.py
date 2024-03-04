91d3fe5fcaddf36b6b34cf378e124f72442fd5e5
RumiYo <yokoyamarumi@gmail.com>
2024-03-04 00:19:28 -0500

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):
+        word_splitted = list(self.word)
+        list_to_return = []
+        for item in words_list:
+            item_splitted = list(item)
+            if sorted(word_splitted) == sorted(item_splitted):
+                list_to_return.append(item)
+        return list_to_return
